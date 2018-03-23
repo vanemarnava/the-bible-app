bef2401b85e9057c2328cb625facb8569425195d
Massienderella <massiel.ams@gmail.com>
2018-03-23 01:49:15 -0300

    render resultados API

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import APIAccess from './APIAccess'
 
 const LandingPage = () =>
   <div>
     <h1>Pagina de inicio</h1>
+     <APIAccess />
   </div>
 
 export default LandingPage;
